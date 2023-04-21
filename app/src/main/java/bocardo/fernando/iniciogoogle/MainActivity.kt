d3b2c1cf4adba89d409b96aa34aa8fd5c7db51ea
Fernando <fernando.bocardo228476@potros.itson.edu.mx>
2023-04-20 23:54:41 -0700

    Fallo corregido (cambiado casteo de Button a SignInButton)

--- app/src/main/java/bocardo/fernando/iniciogoogle/MainActivity.kt
+++ app/src/main/java/bocardo/fernando/iniciogoogle/MainActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
@@ -32,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        val signInButton = findViewById<Button>(R.id.sign_in_button)
+        val signInButton = findViewById<SignInButton>(R.id.sign_in_button)
         signInButton.setOnClickListener {
             val signInIntent = mGoogleSignInClient.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
